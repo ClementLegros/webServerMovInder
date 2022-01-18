2471befb0431cf782b7cb01c565ac640cedccd73
Clement Legros <clement.legros.91220@gmail.com>
2022-01-18 16:07:39 +0100

    Exemple

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
-    res.send('Hello world !!');
+    res.send('Dylan et calvin sont trop beau');
 });
 
 app.listen(port, () => {
