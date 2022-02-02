a122970408ef65bb568ffeb738c794811656ff41
Clément Legros <71426481+ClementLegros@users.noreply.github.com>
2022-02-02 20:28:43 +0100

    UPDATE add postgre

---
+++ db.sql
@@ -0,0 +1,81 @@
+CREATE TABLE utilisateur(
+  id_utilisateur SERIAL,
+  nom_utilisateur VARCHAR(100),
+  e_mail VARCHAR(150),
+  num_utilisateur VARCHAR(10),
+  mdp_utilisateur VARCHAR(250),
+  sexe VARCHAR(10),
+  orientation VARCHAR(50),
+  PRIMARY KEY(id_utilisateur)
+);
+CREATE TABLE message(
+  id_message SERIAL,
+  date_envoie_msg DATETIME,
+  message VARCHAR(350),
+  id_utilisateur INT NOT NULL,
+  PRIMARY KEY(id_message),
+  FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+CREATE TABLE films(
+  id_film SERIAL,
+  titre_film VARCHAR(250),
+  sypnosis VARCHAR(300),
+  directeur VARCHAR(300),
+  affiche VARCHAR(250),
+  note INT,
+  PRIMARY KEY(id_film)
+);
+CREATE TABLE genre(
+  id_genre SERIAL,
+  genre VARCHAR(300),
+  PRIMARY KEY(id_genre)
+);
+CREATE TABLE conversation(
+  id_conversation SERIAL,
+  date_deb_conversation DATETIME,
+  rendez_vous INT,
+  PRIMARY KEY(id_conversation)
+);
+CREATE TABLE poss Ã ¨ de(
+  id_film INT,
+  id_genre INT,
+  PRIMARY KEY(id_film, id_genre),
+  FOREIGN KEY(id_film) REFERENCES films(id_film),
+  FOREIGN KEY(id_genre) REFERENCES genre(id_genre)
+);
+CREATE TABLE est_dans(
+  id_utilisateur INT,
+  id_conversation INT,
+  PRIMARY KEY(id_utilisateur, id_conversation),
+  FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+  FOREIGN KEY(id_conversation) REFERENCES conversation(id_conversation)
+);
+CREATE TABLE contient(
+  id_message INT,
+  id_conversation INT,
+  PRIMARY KEY(id_message, id_conversation),
+  FOREIGN KEY(id_message) REFERENCES message(id_message),
+  FOREIGN KEY(id_conversation) REFERENCES conversation(id_conversation)
+);
+CREATE TABLE aime(
+  id_utilisateur INT,
+  id_film INT,
+  PRIMARY KEY(id_utilisateur, id_film),
+  FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur),
+  FOREIGN KEY(id_film) REFERENCES films(id_film)
+);
+
+/* CREATE USER */
+
+INSERT into
+  utilisateur
+VALUES
+  (
+    1,
+    'Albert',
+    'albert-mail@mail.fr',
+    '0650789962',
+    '123456',
+    'homme',
+    'hétéro'
+  )
--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+//DB postgresql
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: "uxzeurnzypayhy",
+    host: "ec2-54-228-97-176.eu-west-1.compute.amazonaws.com",
+    database: "ddj908o61q0v9g",
+    password: "d6b1db639e1f499f6934bdcab92894bda1109fd964254f2597f03fb7a0d32391",
+    port: 5432,
+    ssl: true
+
+});
+
 //Server Node JS
 
 const express = require('express');
@@ -8,6 +21,29 @@ app.get('/', (req, res) => {
     const json = require('./sample.json');
     res.json(json);
 });
+
+app.get('/user', function (req, res) {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+    pool.query('select * from utilisateur', (error, results) => {
+        if (error) {
+            return console.error(error);
+        }
+        res.status(200).json(results.rows);
+    });
+})
+
+app.get('/login/:username/:password', function (req, res) {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+    const username = req.params.username;
+    const password = req.params.password;
+
+    pool.query('select * from utilisateur where nom_utilisateur = $1 and mdp_utilisateur = $2', [username, password] ,(error, results) => {
+        if (error) {
+            return console.error(error);
+        }
+        res.status(200).json(results.rows);
+    });
+})
 
 app.get('/personnage', (req, res) => {
     const json = require('./sample2.json');
